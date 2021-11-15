fe7752c7b79d35a5d4e0fb9673d137f3c8b04358
warren63 <68918219+warren63@users.noreply.github.com>
2021-11-15 13:24:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+<link rel="stylesheet" >
+<style type="text/css">
+.auto-style1 {
+font-size: large;
+}
+.auto-style2 {
+font-size: larger;
+}
+.auto-style7 {
+text-align: center;
+}
+.auto-style8 {
+text-align: right;
+}
+.auto-style9 {
+text-align: left;
+}
+.auto-style10 {
+text-align: left;
+font-family: Georgia, "Times New Roman", Times, serif;
+}
+#hp  {
+float: right;    
+ margin: 0 0 0 15px;
+}
+</style>
+</head>
+
+<!-- Basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+   
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+ 
+     <!-- Site Metas -->
+    <title>StockFlow - Education & Analysis</title>  
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- ALL VERSION CSS -->
+    <link rel="stylesheet" href="css/versions.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body class="seo_version">
+
+  	<!-- Start header -->
+	<header class="top-navbar">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="container">
+				<a class="navbar-brand" href="index.html">
+					<img src="images/logo-seo.png" alt="" />
+				</a> 
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-seo" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+				  <span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbars-seo">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active"><a class="nav-link" href="index.html"><o class="oblique">HOME</o></a></li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown"><o class="oblique">LEARN</o></a>
+							<div class="dropdown-menu" aria-labelledby="dropdown-a">
+								<a class="dropdown-item" href="terminology.html"><o class="oblique">STOCK TERMINOLOGY</o></a>
+								<a class="dropdown-item" href="stock.html"><o class="oblique">STOCK EDUCATION</o></a>
+								<a class="dropdown-item" href="report.html"><o class="oblique">REPORTS</o></a>
+							
+							</div>
+						</li>
+						<li class="nav-item"><a class="nav-link" href="blog_index.html"><o class="oblique">BLOG</o></a></li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown"><o class="oblique">TIPS</o></a>
+							<div class="dropdown-menu" aria-labelledby="dropdown-a">
+								<a class="dropdown-item" href="chart.html"><o class="oblique">CHARTS & GRAPHS</o></a>
+								<a class="dropdown-item" href="#"><o class="oblique">CASE STUDY</o></a>
+								<a class="dropdown-item" href="#"><o class="oblique">INVESTOR'S IDEAS</o></a>
+							</div>
+						</li>
+                        <li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown"><o class="oblique">KNOWLEDGE CHECK</o></a>
+							<div class="dropdown-menu" aria-labelledby="dropdown-a">
+								<a class="dropdown-item" href="https://formfacade.com/headless/114631148419314701659/home/form/1FAIpQLSeIIs3_zUF_mz_9bNNPK5Gt3k0Ji5eY_hf5MBFNDcC7hCmXJw"><o class="oblique">BSE</o></a>
+								<a class="dropdown-item" href="https://formfacade.com/headless/114631148419314701659/home/form/1FAIpQLSfIUPcNpHQaDVxQJTDulXgCHQ_ROb-d87aRzmcs1P4If6dGpg"><o class="oblique">CAPITAL MARKET</o></a>
+								<a class="dropdown-item" href="https://formfacade.com/headless/114631148419314701659/home/form/1FAIpQLSf_YUCbsqOblXhJXevuakVc_TEnfYKdd4NhWNnxoQU02c8nUA"><o class="oblique">DIVIDENDS & DEBENTURES</o></a>
+                                <a class="dropdown-item" href="https://formfacade.com/headless/114631148419314701659/home/form/1FAIpQLScZZnNlS8XdqeEOYKFjVcf0F_HHEZ7HkLq-GGzPyWx9ScBK5g"><o class="oblique">SENSEX</o></a>
+								<a class="dropdown-item" href="https://formfacade.com/headless/114631148419314701659/home/form/1FAIpQLScQX_rtnxaLBHZZGJdsnoVkKXuaBp4DwgixLaDKj0bqUAVmaw"><o class="oblique">DEMAT & ACCOUNTS</o></a>
+							</div>
+						</li>
+						<li class="nav-item"><a class="nav-link" href="http://localhost:8501/" target="_blank"><o class="oblique">PREDICTOR</o></a></li>
+						<li class="nav-item"><a class="nav-link" href="index.php"><o class="oblique">SUPPORT</o></a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<!-- End header -->
+
+
+
+<p><p>
+
+<body  style="width: 1350px; height: 712px; background-color: #85DDD6;">
+<img src="images/conone.jpg" alt=""  id="hp"/>
+<form class="form-horizontal" style="margin-left:360px; background-color: #5EDBD0; height: 714px; width: 658px;" method="POST" action="contact_query.php">
+<h2 class="auto-style7">
+
+ <span class="auto-style2"><b>Contact Us</b></span>
+</h2><hr>
+
+ <p class="auto-style9">
+ <label for="full_name" style="font-size:18px; font-family:georgia; margin-top:10px;">
+ <strong>Your Name</strong></label></p>
+<p class="auto-style9">
+ &nbsp;<input type="text" name="full_name" id="full_name" placeholder="Enter your full name . . . ." autofocus="autofocus" required style="width: 304px; height: 56px"/>
+ 
+ </p>
+ 
+ <p class="auto-style10">
+ <label for="email" style="font-size:18px; font-family:georgia margin-top:10px;">
+ <strong>Your Email Address</strong></label></p>
+<p class="auto-style9">
+ &nbsp;<input type="email" name="email" id="email" placeholder="Enter your email . . . ." required style="width: 302px; height: 49px"/>
+ 
+ </p>
+ 
+ <p class="auto-style10">
+ <label for="message" style="font-size:18px; font-family:georgia margin-top:10px;">
+ <strong>Your Feedback</strong></label></p>
+<p class="auto-style9">
+ &nbsp;<textarea name="message" placeholder="Enter your feedback . . . ." required style="width: 479px; height: 194px"></textarea>
+ 
+ </p>
+<p class="auto-style7">
+    &nbsp;</p>
+ 
+<div class="auto-style8" style="width: 604px; height: 47px">
+ 
+ 
+ <button type="submit" name="send_message" class="auto-style1" style="width: 91px; height: 42px"><i class="icon-ok icon-large"></i>
+ <strong>SUBMIT</strong></button>
+ </div>
+
+<br />
+<br />
+<br />
+</form>
+<p><p>
+<footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-xs-12">
+                    <div class="widget clearfix">
+                        <div class="widget-title">
+            			 <img src="images/StockFlow_footer.png" alt="">
+                        </div>
+                        
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+				
+				<div class="col-lg-2 col-md-6 col-xs-12">
+                    <div class="widget clearfix">
+                        <div class="widget-title">
+                            <h3>Information</h3>
+                        </div>
+                        <ul class="footer-links">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <!-- <li><a href="#">Pricing</a></li> -->
+							<!-- <li><a href="#">About</a></li> -->
+							<!-- <li><a href="#">Faq</a></li> -->
+							<li><a href="#">Support</a></li>
+                        </ul><!-- end links -->
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+
+                <div class="col-lg-3 col-md-6 col-xs-12">
+                    <div class="widget clearfix">
+						
+						
+                        <div class="widget-title">
+                            <h3>Social</h3>
+                        </div>
+                        <ul class="footer-links social-md">
+                            <li><a class="fb btn-a" title="Facebook" data-tippy-animation="scale" href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a class="gi btn-a" title="Github" data-tippy-animation="scale" href="#"><i class="fa fa-github"></i> </a></li>
+                            <li><a class="tw btn-a" title="Twitter" data-tippy-animation="scale" href="#"><i class="fa fa-twitter"></i> </a></li>
+                            <li><a class="dr btn-a" title="Dribbble" data-tippy-animation="scale" href="#"><i class="fa fa-dribbble"></i> </a></li>
+                            <li><a class="pi btn-a" title="Pinterest" data-tippy-animation="scale" href="#"><i class="fa fa-pinterest"></i> </a></li>
+                        </ul><!-- end links -->
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+				
+				<div class="col-lg-3 col-md-6 col-xs-12">
+                    <div class="widget clearfix">
+                        <div class="widget-title">
+                            <h3>Contact Details</h3>
+                        </div>
+
+                        <ul class="footer-links">
+                            <li><a href="mailto:#">stockflow@gmail.com</a></li>
+                            <li><a href="#">www.stockflow.com</a></li>
+                            <li>+91 123456789</li>
+                        </ul><!-- end links -->
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+				
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </footer><!-- end footer -->
+	
+    <div class="copyrights">
+        <div class="container">
+            <div class="footer-distributed">
+                <div class="footer-left">
+                    <p class="footer-company-name">All Rights Reserved. &copy; 2021 <a href="#">StockFlow</a></p>
+                </div>
+            </div>
+        </div><!-- end container -->
+    </div><!-- end copyrights -->
+
+    <a href="#" id="scroll-to-top" class="dmtop global-radius"><i class="fa fa-angle-up"></i></a>
+
+    <!-- ALL JS FILES -->
+    <script src="js/all.js"></script>
+    <!-- ALL PLUGINS -->
+	<script src="js/tippy.all.min.js"></script>
+    <script src="js/custom.js"></script>
+	
+	<script src="js/jquery.ba-cond.min.js"></script>
+	<script src="js/jquery.slitslider.js"></script>
+	<script type="text/javascript">	
+			$(function() {
+			
+				var Page = (function() {
+
+					var $nav = $( '#nav-dots > span' ),
+						slitslider = $( '#slider' ).slitslider( {
+							onBeforeChange : function( slide, pos ) {
+
+								$nav.removeClass( 'nav-dot-current' );
+								$nav.eq( pos ).addClass( 'nav-dot-current' );
+
+							}
+						} ),
+
+						init = function() {
+
+							initEvents();
+							
+						},
+						initEvents = function() {
+
+							$nav.each( function( i ) {
+							
+								$( this ).on( 'click', function( event ) {
+									
+									var $dot = $( this );
+									
+									if( !slitslider.isActive() ) {
+
+										$nav.removeClass( 'nav-dot-current' );
+										$dot.addClass( 'nav-dot-current' );
+									
+									}
+									
+									slitslider.jump( i + 1 );
+									return false;
+								
+								} );
+								
+							} );
+
+						};
+
+						return { init : init };
+
+				})();
+
+				Page.init();
+			
+			});
+		</script>
+		
+		<script>
+			tippy('.btn-a')
+		</script>
+
+<script src="//code.tidio.co/enh4nw89ddnwwf6rpgaf831fg2xzaxjh.js" async></script>
+
+</html>
